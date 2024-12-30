6604825c434a403eb8b6680ce4a5e86e75eca9db
Yinan Zhou <yinan.a.zhou@gmail.com>
2024-12-29 20:18:53 -0500

    Updates

--- _next/static/chunks/app/layout-752d730a58945e7a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7285:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,347,23)),Promise.resolve().then(a.t.bind(a,3430,23))},347:()=>{},3430:e=>{e.exports={style:{fontFamily:"'Noto Sans', 'Noto Sans Fallback'",fontStyle:"normal"},className:"__className_48ea4f"}}},e=>{var s=s=>e(e.s=s);e.O(0,[176,441,517,358],()=>s(7285)),_N_E=e.O()}]);
